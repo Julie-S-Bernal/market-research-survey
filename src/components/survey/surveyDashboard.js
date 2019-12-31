3c5cf6aa962e6fcc2073f5324e5ec6746bb155f7
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2019-12-31 20:02:11 +0000

    added authentication

---
+++ src/components/survey/surveyDashboard.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function surveyDashboard() {
+    return(
+        //To Do - Add Navbar to handle log out functionality from base
+        <div>Survey Dashboard</div>
+    )
+}
