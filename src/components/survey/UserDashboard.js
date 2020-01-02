2a81371b575a9e49b09148265a3fddd4d18ad761
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2020-01-02 23:09:13 +0000

    added data and some styling

--- src/components/survey/UserDashboard.js
+++ src/components/survey/UserDashboard.js
@@ -1,31 +1,9 @@
 import React, { useState, useEffect } from 'react'; //in order to consume context need to import useContext from React, don't forget!
 import styled from 'styled-components';
 import { Container, Col, Row } from 'styled-bootstrap-grid';
-
+import { VictoryPie } from 'victory';
 import AuthenticationButtons from '../userAuthentication/authenticationButtons'
-//the actual data that will be consumed
 
-const StyledContainer= styled.div`
-  align-items: center;
-  border-radius: 5px;
-  background: #6d658f;
-  width: 100%;
-  padding: 40px;
-  text-decoration: none;
-  box-shadow: -20px -20px 0px 0px rgba(100,100,100, 0.1);
-  `;
-
-const Spacing= styled.div`
-  margin: 10px;
-  background-color: red;
-`;
-
-const Separator = styled.div`
-  background-color: #9485d4;
-  width: 100%;
-  margin-top: 10px;
-  height: 1px;
-`
 
 const SurveyDashboard = () => {
   const [data, setDataSet] = useState([]);
@@ -44,54 +22,63 @@ const SurveyDashboard = () => {
     fetchData();
   },[]);
 
-
- console.log(data && data)
- console.log(data.surveys)
-
-
-// this will return the city from the first address item.
-
   return(
-    <Container >
+    <Container>
       <Row>
-        <Col col={6}>
-        <AuthenticationButtons />
-        <button className='Environment' >Environment</button>
-        <button className='Brexit'>Brexit</button>
-        <button className='US'>US Elections</button>
-        <div>
-
-        <div>
-        {
-           data.length != 0 && data.surveys.map(({title, questions}, i) => (
-            <div key={i}>
-              <div>
-                <h1>{title}</h1>
-                {console.log('hi', questions.length != 0 && JSON.stringify(questions))}
-                <>
-                  {questions.map((question, j) => (
-                  <div key={j}>
-                    <h1>{question.questionId}</h1>
-                    <h1>{question.questionTitle}</h1>
-                    <>
-                    {question.answerOptions.map((answer, questionId, answerOptions, j) => (
-                  <div key={j}>
-                    <h1>{answer.answerOptions}</h1>
-                    <h1>{answer.text}</h1>
-                    <h1>Selected by respondent: {answer.selectedByRespondents}</h1>
-                  </div>))}
-                  </>
-                  </div>))}
-                  </>
-              </div>
-            </div>
-          ))
-        }
-       </div>
-        </div>
+        <Col col={12}>
+          <AuthenticationButtons />
         </Col>
       </Row>
-    </Container>
+      <Row>
+        <Col col={12}>
+          <button className='Environment'>Environment</button>
+          <button className='Brexit'>Brexit</button>
+          <button className='US'>US Elections</button>
+        </Col>
+      </Row>
+      <Row>
+        <Col col={12}>
+          <div>
+            <div>
+              { data.length !== 0 && data.surveys.map(({title, questions}, i) => (
+                <div key={i}>
+                  <div>
+                    <h1>{title}</h1>
+                    <>
+                      {questions.map((question, key, j) => (
+                      <div key={j}>
+                        <h2>{question.questionTitle}</h2>
+                        <div style={{width:'250px', height: '250px'}}>
+                        <VictoryPie
+                        colorScale={['#cf4475', '#f79ebd', '#c3e3e8', '#b0f08b', '#6fc969' ]}
+                        data={[
+                          { x: questions[key].answerOptions[0].text, y: questions[0].answerOptions[0].selectedByRespondents },
+                          { x: questions[key].answerOptions[1].text, y: questions[0].answerOptions[1].selectedByRespondents },
+                          { x: questions[key].answerOptions[2].text, y: questions[0].answerOptions[2].selectedByRespondents },
+                          { x: questions[key].answerOptions[3].text, y: questions[0].answerOptions[1].selectedByRespondents },
+                          { x: questions[key].answerOptions[4].text, y: questions[0].answerOptions[2].selectedByRespondents },
+                        ]}
+                      />
+                      </div>
+                        <>
+                          {question.answerOptions.map((answer, questionId, answerOptions, k) => (
+                            <div key={k}>
+                              {/* <h3>{answer.answerOptions}</h3> */}
+                              <p>{answer.text}</p>
+                              <p>Selected by respondent: <span>{answer.selectedByRespondents}</span></p>
+                            </div>
+                          ))}
+                        </>
+                      </div>))}
+                    </>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </Col>
+      </Row>
+  </Container>
   )
 }
 
